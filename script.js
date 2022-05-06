8e4abe4a1c2c6a47df3d216ccfdd19b24f578f95
Josh <jdodge.programming@gmail.com>
2022-05-06 12:43:38 -0400

    Function added to play a round

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-//Set a function called computer play. This will randomly return rock, paper or scissors
-let computerChoice = 0
-let userChoice = prompt("Please select rock, paper or scissors:");
+//Set reused vars.
+let playerSelection = prompt('Please choose rock, paper or scissors:');
+computerSelection = computerPlay();
 
+//Set a function called computer play. This will randomly return rock, paper or scissors
 function computerPlay() {
-  computerChoice = Math.floor(Math.random() * 3);
-  if (computerChoice === 0) {
-    console.log("Rock");
+  let computerChoice = Math.floor(Math.random() * 3);
+  if(computerChoice === 0) {
+    return 'rock';
   } else if (computerChoice === 1) {
-    console.log("Paper");
+    return 'paper';
   } else {
-    console.log("Scissors");
+    return 'scissors';
   }
 }
+
+//Function to play a round.
+function playRound() {
+  if (playerSelection === 'rock' && computerSelection === 'rock') {
+    alert('Its a tie');
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    alert('You lost! Paper beats rock.');
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    alert('You won! Rock beats scissors.');
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    alert('You won! Paper beats rock.');
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    alert('It is a tie.');
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    alert('You lost. Scissors beats paper')
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    alert('You lost. Rock beats scissors');
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    alert('You won. Scissors beats paper');
+  } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    alert('It is a tie.')
+  } else {
+    alert('You are a dumbass. Enter rock, paper or scissors');
+  }
+}
+
+playRound();
